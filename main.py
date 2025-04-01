c30175f76cc364fe3e382d4d30cc51a34ea1285c
avarity <72452505+avarity@users.noreply.github.com>
2025-04-01 19:57:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import sys
 from stats import get_num_words, count_characters, sort_characters
 
 def get_book_text(path):
-
     with open(path, 'r', encoding='utf-8') as f:
         text = f.read()
     
+    # Пропускаем метаданные Project Gutenberg
     start = text.find("*** START OF THE PROJECT GUTENBERG EBOOK") + 1
     end = text.find("*** END OF THE PROJECT GUTENBERG EBOOK")
     return text[start:end].strip()
@@ -20,14 +21,21 @@ def print_report(book_path, word_count, sorted_chars):
     print("============= END ===============")
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     
-    word_count = get_num_words(text)
-    char_counts = count_characters(text)
-    sorted_chars = sort_characters(char_counts)
+    book_path = sys.argv[1]
     
-    print_report(book_path, word_count, sorted_chars)
+    try:
+        text = get_book_text(book_path)
+        word_count = get_num_words(text)
+        char_counts = count_characters(text)
+        sorted_chars = sort_characters(char_counts)
+        print_report(book_path, word_count, sorted_chars)
+    except FileNotFoundError:
+        print(f"Error: File '{book_path}' not found")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
