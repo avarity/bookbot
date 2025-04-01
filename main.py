5a5d0355f59209d06235998a6c78b9f46c29e05b
avarity <miawoowo@gmail.com>
2025-04-01 19:50:43 +0300

    add formatted report with sorted character counts

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-from stats import get_num_words, count_characters
+from stats import get_num_words, count_characters, sort_characters
 
 def get_book_text(path):
+
     with open(path, 'r', encoding='utf-8') as f:
-        return f.read()
+        text = f.read()
+    
+    start = text.find("*** START OF THE PROJECT GUTENBERG EBOOK") + 1
+    end = text.find("*** END OF THE PROJECT GUTENBERG EBOOK")
+    return text[start:end].strip()
+
+def print_report(book_path, word_count, sorted_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     
-    print(f"{get_num_words(text)} words found in the document")
-    
+    word_count = get_num_words(text)
     char_counts = count_characters(text)
-    print("\nCharacter frequencies:")
-    for char, count in sorted(char_counts.items()):
-        print(f"'{char}': {count}")
+    sorted_chars = sort_characters(char_counts)
+    
+    print_report(book_path, word_count, sorted_chars)
 
 if __name__ == "__main__":
     main()
